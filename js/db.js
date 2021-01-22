eee87a40ba5928d75d62e82077c9bf1ca382c47b
hasnarof <hasnarof@gmail.com>
2021-01-22 19:00:30 +0700

    lesson-25 realtime data updated

--- js/db.js
+++ js/db.js
@@ -1,8 +1,9 @@
 // real-time listener
       /*
-      real time listener ketika ada data di database yang di add / di hapus.
+      real time listener ketika ada data di database yang di add / di hapus (perubahan di database)
+      onSnapshot merupakan method yang dapat me listen perubahan pada collection recipes. kemudian ada snaphot, yaitu perubahan2nya.
       */
-db.collection('recipes').onSnapshot(snapshot => { // collection == table. onSnapshot untuk tau snaphot2 perubahan2 itu
+db.collection('recipes').onSnapshot(snapshot => { // collection == table.
   //console.log(snapshot.docChanges());
   snapshot.docChanges().forEach(change => { // docChanges ngelist perubahan2 itu... change.doc.data (buat tau data)
                                             // change.doc.id buat tau id
