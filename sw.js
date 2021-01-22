cabc5a29ec2ae138e0985dadaf85533705239a15
hasnarof <hasnarof@gmail.com>
2021-01-22 11:12:48 +0700

    lesson-17 cache versioning: menghapus cache yang dulu dan hanya memakai yg sekarang

--- sw.js
+++ sw.js
@@ -32,14 +32,22 @@ self.addEventListener('install', evt => {
 });
 
 // activate event
+
+  /*
+    nah jadi di sini kita mau bikin cache baru, liat nama staticCacheName nya jadi versi dua. nah pas diinstall dan di activate itu
+    bakal ada dua cache nanti yg terdaftar. jadi kalau misal kita mau ambil index.html di cache v2 malah keambil di cache pertama.
+    jadi caranya adalah menghapus semua cache yang bukan nama cache sekarang.
+    
+    
+  */
 self.addEventListener('activate', evt => {
   //console.log('service worker activated');
-  evt.waitUntil(
-    caches.keys().then(keys => {
+  evt.waitUntil(  // promise akan menyelesaikan semuanya sebelum fungsi evt => selesai
+    caches.keys().then(keys => { // caches.keys() mendapatkan semua nama cache, jika berhasil maka callback fungsi lagi dgn argumen keys
       //console.log(keys);
-      return Promise.all(keys
-        .filter(key => key !== staticCacheName)
-        .map(key => caches.delete(key))
+      return Promise.all(keys // promise semua key akan di filter
+        .filter(key => key !== staticCacheName) // fungsi callback lagi, return true untuk key != staticCacheName
+        .map(key => caches.delete(key)) // hasil filteran akan dimap, dihapus key yang dalam filteran.
       );
     })
   );
