5e6c08e77b34b078c6d280dc62c5faa115e86b8e
hasnarof <hasnarof@gmail.com>
2021-01-22 17:17:22 +0700

    lesson-24 Realtime Data

--- js/db.js
+++ js/db.js
@@ -1,7 +1,11 @@
 // real-time listener
-db.collection('recipes').onSnapshot(snapshot => {
+      /*
+      real time listener ketika ada data di database yang di add / di hapus.
+      */
+db.collection('recipes').onSnapshot(snapshot => { // collection == table. onSnapshot untuk tau snaphot2 perubahan2 itu
   //console.log(snapshot.docChanges());
-  snapshot.docChanges().forEach(change => {
+  snapshot.docChanges().forEach(change => { // docChanges ngelist perubahan2 itu... change.doc.data (buat tau data)
+                                            // change.doc.id buat tau id
     //console.log(change.type, change.doc.id, change.doc.data());
     if(change.type === 'added'){
       // add the document data to the web page
